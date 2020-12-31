6719eb415ff5858b404b1a6829d8f90eaa0b5674
sarapourabbas <sara.poorabas@gmail.com>
2020-12-31 17:03:50 +0330

    lunching project with ejs

---
+++ server.js
@@ -0,0 +1,26 @@
+var data =require ('./data/data.json');
+// load the things we need
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// Static Files
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/images', express.static(__dirname + 'public/images'))
+app.use('/fonts', express.static(__dirname + 'public/fonts'))
+
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+    res.render('pages/index',data);
+});
+
+
+app.listen(8080);
+console.log('8080 is the magic port');
